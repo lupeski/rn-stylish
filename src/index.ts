e008bdd03f5b88272d3342e8ee34f898d838433e
Tony Lupeski <lupeski@gmail.com>
2025-12-09 09:09:47 -0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 export {createThemedStyles} from './createThemedStyles';
 export {useThemeSelect} from './useThemeSelect';
-export {
-	themeModeAtom,
-	lightThemeStylesAtom,
-	darkThemeStylesAtom,
-	staticStylesAtom,
-} from './themeAtom';
+
 export type {Theme, ThemeMode, ThemeStyles, StaticStyles} from './types';
