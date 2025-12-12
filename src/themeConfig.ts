892af93603e2104ab5f0a17c164bdb9a0a82ad9e
Tony Lupeski <lupeski@gmail.com>
2025-12-12 16:40:24 -0700

    Update

--- src/themeConfig.ts
+++ src/themeConfig.ts
@@ -94,8 +94,10 @@ export function configureTheme<
 			props?: Props
 		): ThemedStylesHook<Styles, ThemeStylesType, StaticStylesType> => {
 			const systemScheme = useColorScheme();
-			const mode = useAtomValue(themeModeAtom);
-			const themeVersion = useAtomValue(themeVersionAtom, {store});
+			// const mode = useAtomValue(themeModeAtom);
+			// const themeVersion = useAtomValue(themeVersionAtom, {store});
+			let mode = 'system';
+			const themeVersion = 0;
 
 			// Memoize activeTheme to prevent unnecessary re-renders
 			const activeTheme = useMemo(() => {
